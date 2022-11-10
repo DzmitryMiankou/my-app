5e60b6534ee3df504547bd4dac099f757e0f708b
DzmitryMiankou <gmiankou@gmail.com>
2022-11-10 11:00:20 +0300

    yes

--- src/components/redux/state.js
+++ src/components/redux/state.js
@@ -1,29 +1,31 @@
-import { renderRoot } from "../../render";
+let renderRoot;
 
 let state = {
   messegData: [],
   newChanges: "",
 };
 
-export let addPost = () => {
-  let a = state.newChanges;
-  if (a === "") {
+export const addPost = () => {
+  if (state.newChanges === "") {
     return;
   }
   let newPost = {
     id: 5,
-    messege: a,
+    messege: state.newChanges,
     like: 23,
   };
   state.messegData.push(newPost);
   upDateChange("");
-  renderRoot(state);
-  console.log(state);
+  renderRoot();
 };
 
-export let upDateChange = (newChange) => {
+export const upDateChange = (newChange) => {
   state.newChanges = newChange;
-  renderRoot(state);
+  renderRoot();
+};
+
+export let subscribe = (observer) => {
+  renderRoot = observer;
 };
 
 export default state;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import { renderRoot } from "./render.js";
-import state from "./components/redux/state.js";
+import state, { subscribe } from "./components/redux/state.js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { addPost, upDateChange } from "./components/redux/state";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let renderRoot = () => {
+  root.render(
+    <React.StrictMode>
+      <App appState={state} toRedux={addPost} upDate={upDateChange} />
+    </React.StrictMode>
+  );
+};
 
 renderRoot(state);
+subscribe(renderRoot);
+reportWebVitals();
