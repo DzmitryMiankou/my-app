5d73136da1ecf7939d232c0f07de532f882a392c
DzmitryMiankou <gmiankou@gmail.com>
2022-11-20 13:50:52 +0300

    good style stars

--- src/components/main/myWorks/Works.jsx
+++ src/components/main/myWorks/Works.jsx
@@ -3,14 +3,20 @@ import styleWorks from "./Works.module.css";
 import Stars from "./stars/Stars";
 import Jpg from "../../../img/20zs01.jpg";
 const Works = () => {
-  let createArray = [1, 2, 3, 4, 5];
-  const copyStar = createArray.map((e, i) => <Stars key={i} />);
-
+  const stars = [];
+  stars.length = 5;
+  const createStars = [...stars];
+  const copyStar = createStars.map((e, i) => <Stars key={i} />);
+  const df = () => {
+    alert("hi");
+  };
   return (
     <div className={styleWorks.container}>
       <h2 className={styleWorks.text}>Мои работы</h2>
       <img src={Jpg} alt="jpg" width="50%" />
-      <div className={styleWorks.stars}>{copyStar}</div>
+      <div onClick={df} className={styleWorks.stars}>
+        {copyStar}
+      </div>
       <h2 className={styleWorks.text}>Работы учеников</h2>
     </div>
   );
--- src/components/main/myWorks/stars/Stars.jsx
+++ src/components/main/myWorks/stars/Stars.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { AiFillStar } from "@react-icons/all-files/ai/AiFillStar";
+import { createUseStyles } from "react-jss";
+
+const styles = createUseStyles({
+  star: {
+    color: "#c9d1d9",
+    cursor: "pointer",
+    "&:hover": {
+      color: "#dc5b24",
+    },
+  },
+});
 
 const Stars = () => {
+  const classes = styles();
   return (
     <div>
-      <AiFillStar color="#aa4f28" size={"30px"} />
+      <AiFillStar className={classes.star} size={"30px"} />
     </div>
   );
 };
