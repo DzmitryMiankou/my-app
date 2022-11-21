9ce24f1457d22192fdaa0fc7bf42ed34a74171ce
DzmitryMiankou <gmiankou@gmail.com>
2022-11-21 13:57:10 +0300

    lk

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import style from "./Header.module.css";
 import Link from "./link/Link.jsx";
 import Logo from "./logo/Logo";
+import { GrLanguage } from "@react-icons/all-files/gr/GrLanguage";
 
 const Header = () => {
+  const styleLang = () => {
+    alert("hi");
+  };
   return (
     <header className={style.header}>
       <div>
@@ -15,6 +19,15 @@ const Header = () => {
           <Link text="Портфолио" to="/works" />
           <Link text="Обучение" to="/learning" />
           <Link text="Общение" to="/chat" />
+          <GrLanguage
+            onClick={styleLang}
+            size={"25px"}
+            className={style.lang}
+          />
+          <ul className={style.lang_list} style={{ display: "none" }}>
+            <li>Беларуская</li>
+            <li>Русский</li>
+          </ul>
         </ul>
       </menu>
     </header>
