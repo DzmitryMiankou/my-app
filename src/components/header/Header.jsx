2469ece3bd1fe5567b608f0bebd006dc20b3cd13
DzmitryMiankou <gmiankou@gmail.com>
2022-12-01 11:33:07 +0300

    placeholder

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import style from "./Header.module.css";
 import Link from "./link/Link.jsx";
 import Logo from "./logo/Logo";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const [lang, setLang] = useState(false);
@@ -49,7 +50,7 @@ const Header = () => {
               <li>Русский</li>
             </ul>
           </div>
-          <a href="/regist">
+          <NavLink to="/regist">
             <svg
               className={style.reg}
               width="25"
@@ -60,7 +61,7 @@ const Header = () => {
               <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
               <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
             </svg>
-          </a>
+          </NavLink>
         </ul>
       </menu>
     </header>
--- src/components/main/registration/Registration.jsx
+++ src/components/main/registration/Registration.jsx
@@ -9,19 +9,33 @@ const Registration = () => {
           <label className={styleRegistration.label} htmlFor="nickName">
             Nickname
           </label>
-          <input id="nickName" type="text" />
+          <input placeholder="Например: Explore23" id="nickName" type="text" />
         </div>
         <div className={styleRegistration.elem__container}>
           <label className={styleRegistration.label} htmlFor="email">
             Email
           </label>
-          <input id="email" type="text" />
+          <input
+            placeholder="Например: explore@gmail.com"
+            id="email"
+            type="text"
+          />
         </div>
         <div className={styleRegistration.elem__container}>
-          <label className={styleRegistration.label} htmlFor="password">
+          <label
+            placeholder="Например: explore@gmail.com"
+            className={styleRegistration.label}
+            htmlFor="password"
+          >
             Password
           </label>
-          <input id="password" type="text" />
+          <input placeholder="Например: 230cd_3rD" id="password" type="text" />
+        </div>
+        <div
+          placeholder="Например: explore@gmail.com"
+          className={styleRegistration.elem__container}
+        >
+          <button className={styleRegistration.button}>Регистрация</button>
         </div>
       </form>
     </div>
