7c1642be558fac0fe7c35a77cb2a80a5959605b6
DzmitryMiankou <gmiankou@gmail.com>
2022-12-03 10:40:00 +0300

    as

--- src/hook/useAddList.js
+++ src/hook/useAddList.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 
 export const useAddList = (bool, elem) => {
+  window.addEventListener("DOMFocusIn", (event) => {
+    console.log("Resized");
+  });
   const [value, setValue] = useState(bool);
 
   const add = () => {
