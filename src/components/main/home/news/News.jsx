1c1e69c3340d069d59accbf1775ddb3a92afef27
DzmitryMiankou <gmiankou@gmail.com>
2022-12-19 22:50:00 +0300

    weew

--- src/components/main/home/news/News.jsx
+++ src/components/main/home/news/News.jsx
@@ -1,8 +1,13 @@
 import styleNews from "./News.module.scss";
 import React from "react";
+import css from "./../../../.././img/new/css.png";
 
 const News = () => {
-  return <div className={styleNews.container}></div>;
+  return (
+    <div className={styleNews.container}>
+      <img src={css} alt="css" />
+    </div>
+  );
 };
 
 export default News;
